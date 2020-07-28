ccd064a5c75564d4ce1c7690c5c34647b31fbd8c
Diya Mohan <diyajmohan@gmail.com>
2020-07-28 14:20:04 +0000

    template gang

--- app.py
+++ app.py
@@ -1,46 +1,22 @@
-# ---- YOUR APP STARTS HERE ----
-# -- Import section --
 from flask import Flask
 from flask import render_template
-from flask import request
-# from flask_pymongo import PyMongo
-
+from flask import request, redirect, url_for, session
+from datetime import datetime
+import os
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+app.config['PLACES_KEY'] = "AIzaSyB_VVeMFaOFWa9_AuOjHX-zy6N0Y9txPoE"
 
-# name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
-
-# URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
-
-# mongo = PyMongo(app)
-
-# -- Routes section --
-# INDEX
-
+# -- Routes --
 @app.route('/')
-@app.route('/index')
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html', time = datetime.now())
 
-def index():
-    return render_template('index.html', events = events)
 
-
-# CONNECT TO DB, ADD DATA
-
-@app.route('/add')
-
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
+@app.route('/description', methods = ['POST', 'GET'])
+def description():
+    if request.method == 'POST':
+        return render_template('description.html', time = datetime.now())
