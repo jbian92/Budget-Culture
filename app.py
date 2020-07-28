957c42b261d4d3f4873717835619ce0b989383a1
Diya Mohan <diyajmohan@gmail.com>
2020-07-28 15:41:26 +0000

    jinja genre sections

--- app.py
+++ app.py
@@ -4,16 +4,33 @@ from flask import request, redirect, url_for, session
 from datetime import datetime
 #import os
 
+
 # -- Initialization section --
 app = Flask(__name__)
 
 app.config['PLACES_KEY'] = "AIzaSyB_VVeMFaOFWa9_AuOjHX-zy6N0Y9txPoE"
 
+
+# -- Variables --
+genres = [
+    "amusement_park", "aquarium", "art_gallery", 
+    "bakery" , "bar" , "book_store" , "bowling_alley", 
+    "cafe", "campground", "casino", "clothing_store", "convenience_store", 
+    "department_store", "drugstore", 
+    "electronics_store", 
+    "furniture_store", 
+    "hair_care", "home_goods_store", 
+    "jewelry_store", 
+    "library", "liquor_store", 
+    "movie_theater"
+]
+
+
 # -- Routes --
 @app.route('/')
 @app.route('/homepage')
 def homepage():
-    return render_template('homepage.html', time = datetime.now())
+    return render_template('homepage.html', genres = genres, time = datetime.now())
 
 
 @app.route('/description', methods = ['POST', 'GET'])
